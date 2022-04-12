aff0bf6b2428df26e66ab4eed6f0b239189a8701
Nguyen Tien Phong <42340547+TienPhongDX@users.noreply.github.com>
2022-04-12 22:38:35 +0700

    Update books.js

--- src/context/books.js
+++ src/context/books.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import { API, graphqlOperation } from "aws-amplify";
 import { v4 as uuidv4 } from "uuid";
 import { listBooks } from "../graphql/queries";
-import { processOrder } from "../graphql/mutations";
+// import { processOrder } from "../graphql/mutations";
 
 const BookContext = React.createContext();
 
@@ -15,18 +15,18 @@ const BookProvider = ({ children }) => {
     fetchBooks();
   }, []);
 
-  const checkout = async (orderDetails) => {
-    const payload = {
-      id: uuidv4(),
-      ...orderDetails
-    };
-    try {
-      await API.graphql(graphqlOperation(processOrder, { input: payload }));
-      console.log("Order is successful");
-    } catch (err) {
-      console.log(err);
-    }
-  };
+  // const checkout = async (orderDetails) => {
+  //   const payload = {
+  //     id: uuidv4(),
+  //     ...orderDetails
+  //   };
+  //   try {
+  //     await API.graphql(graphqlOperation(processOrder, { input: payload }));
+  //     console.log("Order is successful");
+  //   } catch (err) {
+  //     console.log(err);
+  //   }
+  // };
 
   const fetchBooks = async () => {
     try {
@@ -49,7 +49,7 @@ const BookProvider = ({ children }) => {
   };
 
   return (
-    <BookContext.Provider value={{ books, featured, loading, checkout }}>
+    <BookContext.Provider value={{ books, featured, loading }}>
       {children}
     </BookContext.Provider>
   );
