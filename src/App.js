774c81e93f7634553135c6b3bdd9cbaacd3bd3b6
Nguyen Tien Phong <42340547+TienPhongDX@users.noreply.github.com>
2022-04-12 23:00:09 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 // Amplify
-import Amplify from "aws-amplify";
+import {Amplify} from "aws-amplify";
 
 // Pages
 import Home from "./pages/Home"
@@ -16,8 +16,8 @@ import Admin from './pages/Admin';
 import Header from "./components/Header"
 
 // Amplify Configurations
-import awsExports from "./aws-exports";
-Amplify.configure(awsExports);
+import awsconfig from "./aws-exports";
+Amplify.configure(awsconfig);
 
 
 const App = () => {
